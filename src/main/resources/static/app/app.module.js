abe827cbbf95591db651f654879aadf6eee5829e
bigslugger19 <aaron.g.jones@gmail.com>
2015-12-29 08:24:55 -0700

    UI prototyping

--- src/main/resources/static/app/app.module.js
+++ src/main/resources/static/app/app.module.js
@@ -7,74 +7,20 @@ window.angularModuleName = 'CCC';
         'ui.router',
         'ui.bootstrap',
         'LocalForageModule',
-        /*'angular-web-notification',*/
-        'ngStomp',
-        'ui.select',
-        /*'chart.js',*/
-        'ngToast'
+        'ui.select'
     ])
         .config(config)
         .run(run);
 
-    config.$inject = ['$compileProvider', 'ngToastProvider'];
+    config.$inject = [];
 
-    function config($compileProvider, ngToastProvider) {
-
-        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|tel|file|blob):/);
-
-        ngToastProvider.configure({
-            horizontalPosition: 'right',
-            verticalPosition: 'bottom',
-            dismissOnClick: true
-        });
+    function config() {
+    	// TODO: config stuff
     }
 
-    run.$inject = ['$rootScope', '$log', '$http', '$state', 'AuthService'];
+    run.$inject = ['$rootScope', '$log'];
 
-    function run($rootScope, $log, $http, $state, AuthService) {
-        var bypass = false,
-            checkInstall = true;
-        $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
-            if (bypass) {
-                bypass = false;
-                return;
-            }
-            event.preventDefault();
-            bypass = true;
-
-            if (checkInstall) {
-                return $http.get('/auth/check-install').then(function () {
-                    checkInstall = false;
-                    doNavigation(toState, toParams);
-                }, function () {
-                    AuthService.logout(function () {
-                        return $state.go('setup');
-                    });
-                });
-            }
-
-            doNavigation(toState, toParams);
-
-        });
-
-        function doNavigation(toState, toParams) {
-            if (toState.name === 'setup') {
-                return $state.go('login');
-            }
-
-            AuthService.checkAuthentication(function (isAuthenticated) {
-                if (!isAuthenticated && (!toState.data || !toState.data.insecure)) {
-                    return $state.go('login');
-                }
-
-                if (isAuthenticated && toState.name === 'login') {
-                    return $state.go('shell.dashboard');
-                }
-
-                if (toParams)
-                    return $state.go(toState.name, toParams);
-                return $state.go(toState.name);
-            });
-        }
+    function run($rootScope, $log) {
+    	// TODO: run stuff
     }
 }());
--- src/main/resources/static/app/app.routes.js
+++ src/main/resources/static/app/app.routes.js
@@ -9,53 +9,24 @@
     function config($locationProvider, $httpProvider, $stateProvider, $urlRouterProvider) {
 
         $locationProvider.html5Mode(true);
-
-        $urlRouterProvider.otherwise('/');
-
+        
+        $urlRouterProvider.otherwise('/start');
+        
         $stateProvider
-            .state('login', {
-                url: '/login',
-                templateUrl: '/app/login/login.html',
-                controller: 'LoginController',
-                controllerAs: 'login',
-                data: {
-                    insecure: true
-                }
-            })
-            .state('register', {
-                url: '/register',
-                templateUrl: '/app/register/register.html',
-                controller: 'RegisterController',
-                controllerAs: 'register',
-                data: {
-                    insecure: true
-                }
-
-            })
-            .state('shell', {
-                templateUrl: '/app/layout/shell.html',
-                controller: 'ShellController',
-                controllerAs: 'shell'
-            })
-            .state('shell.dashboard', {
-                url: '/',
-                templateUrl: '/app/dashboard/dashboard.html',
+            
+            // HOME STATES AND NESTED VIEWS ========================================
+            .state('start', {
+                url: '/start',
+                templateUrl: 'app/dashboard/dashboard.html',
                 controller: 'DashboardController',
-                controllerAs: 'dc',
-                data: {
-                    title: 'Dashboard'
-                }
+                controllerAs: 'dashboard'
             })
-            .state('shell.alerts', {
-                url: '/alerts',
-                templateUrl: '/app/alerts/alerts.html',
-                controller: 'AlertsController',
-                controllerAs: 'ac',
-                data: {
-                    title: 'Alerts'
-                }
-            });;
+            
+            // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+            .state('about', {
+                // we'll get to this in a bit       
+            });
 
         $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-    }
+    };
 }());
