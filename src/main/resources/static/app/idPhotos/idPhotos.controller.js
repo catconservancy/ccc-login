c5014c928d867629d8231c16229282186e5b5285
bigslugger19 <aaron.g.jones@gmail.com>
2016-03-26 14:51:21 -0600

    filter menu fixes

--- src/main/resources/static/app/idPhotos/idPhotos.controller.js
+++ src/main/resources/static/app/idPhotos/idPhotos.controller.js
@@ -2,9 +2,9 @@
     angular.module('CCC')
         .controller('IdPhotosController', IdPhotosController);
 
-    IdPhotosController.$inject = ['$log', '$scope', 'PhotosService', 'Species', 'Dropbox'];
+    IdPhotosController.$inject = ['$log', '$scope', 'Species', 'Dropbox'];
 
-    function IdPhotosController($log, $scope, PhotosService, Species, Dropbox) {
+    function IdPhotosController($log, $scope, Species, Dropbox) {
         var vm = this;
         vm.photos = [];
         vm.selectedPhoto = {};
@@ -18,13 +18,6 @@
         vm.thumbClass = thumbClass;
         vm.selectFolder = selectFolder;
         vm.splitFolder = splitFolder;
-
-        PhotosService.getMockedPhotos({},function(data) {
-            vm.photos = data;
-            vm.photos[0].selected = true;
-            vm.selectedPhoto = vm.photos[0];
-            $scope.$on('$viewContentLoaded', initializeCarousel);
-        });
 
         Dropbox.query({},function(data) {
         	for (i = 0; i < data.length; i++) {
@@ -46,8 +39,7 @@
         	      e.stopPropagation();
         	    }
         	});
-        	
-//        	initializeFilters(); 
+        	initializeCarousel();
         });
 
         Species.query(function(data) {
@@ -91,7 +83,6 @@
             	
                 vm.photos[0].selected = true;
                 vm.selectedPhoto = vm.photos[0];
-                $scope.$on('$viewContentLoaded', initializeCarousel);
 	        });
         }
         
@@ -134,45 +125,6 @@
                 	$('#myCarousel').carousel('prev');
                 }
             });
-            
-//            initializeFilters();
-        }
-        
-        function initializeFilters() {
-	    	$('#tree').treeview({data: vm.treeData});
-	    	$('#myDropdown .dropdown-menu').on({
-	    		"click":function(e){
-	    	      e.stopPropagation();
-	    	    }
-	    	});
-	    	$('#tree').on('nodeExpanded', function(event, expandedNode) {
-	    		$log.debug("vm.treeData", vm.treeData);
-	    		$log.debug("nodeExpanded", expandedNode);
-	    		$log.debug("vm.treeData[expandedNode.parentId]", vm.treeData[expandedNode.parentId]);
-	    		$log.debug("vm.treeData[expandedNode.nodeId]", vm.treeData[expandedNode.nodeId]);
-
-    	        Dropbox.query({path: expandedNode.path},function(data) {
-    	        	fetchedNodes = [];
-    	        	vm.fileList = [];
-    	        	for (i = 0; i < data.length; i++) {
-    	        		if (data[i].dir) {
-    	        			fetchedNodes.push({
-    	        				text: data[i].name,
-    	        				path: data[i].pathLower,
-    	        				state: {expanded: false},
-    	        				nodes: [{}]
-    	        			});
-    	        		} else {
-    	        			vm.fileList.push(data[i]);
-    	        		}
-    	        	}
-    	        	vm.treeData[expandedNode.nodeId].state = {expanded: true};
-    	        	vm.treeData[expandedNode.nodeId].nodes = fetchedNodes;
-    	        	$log.debug("vm.fileList", vm.fileList);
-    	        	
-    	        	initializeFilters(); 
-    	        });
-    		});
         }
 
         function setSelectedPhotoByIndex(index) {
