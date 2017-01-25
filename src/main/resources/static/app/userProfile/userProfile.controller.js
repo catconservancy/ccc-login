b09a7dee4618e6e640291eacecf204ce327b53e0
Jones, Aaron (CORP) <Aaron.Jones@adp.com>
2017-01-25 13:12:58 -0700

    User managment and display updates

---
+++ src/main/resources/static/app/userProfile/userProfile.controller.js
@@ -0,0 +1,10 @@
+(function () {
+    angular.module('CCC')
+        .controller('FolderLocationController', FolderLocationController);
+
+    FolderLocationController.$inject = [];
+
+    function FolderLocationController() {
+        var vm = this;
+    }
+}());
