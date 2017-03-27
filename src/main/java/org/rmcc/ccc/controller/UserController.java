acd3a677a4bea8584cac085a12f09bc74a3cd812
Jones, Aaron (CORP) <Aaron.Jones@adp.com>
2017-03-27 13:25:15 -0600

    implement sendgrid email add-on for registration notifications

--- src/main/java/org/rmcc/ccc/controller/UserController.java
+++ src/main/java/org/rmcc/ccc/controller/UserController.java
@@ -1,5 +1,12 @@
 package org.rmcc.ccc.controller;
 
+import com.sendgrid.Content;
+import com.sendgrid.Email;
+import com.sendgrid.Mail;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
 import org.rmcc.ccc.annotations.Loggable;
 import org.rmcc.ccc.model.User;
 import org.rmcc.ccc.model.UserCreateForm;
@@ -11,7 +18,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -23,9 +29,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
 import javax.validation.Valid;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -118,33 +123,68 @@ public class UserController {
 
 	// send a user activated email to user
     private void sendUserActivatedEmail(User user) {
-        MimeMessage mail = javaMailSender.createMimeMessage();
-        try {
-            MimeMessageHelper helper = new MimeMessageHelper(mail, true);
-            helper.setTo(user.getEmail());
-            helper.setReplyTo(ADMIN_EMAIL);
-            helper.setFrom(ADMIN_EMAIL);
-            helper.setSubject("CCC User Access Granted");
-            helper.setText("Your access request has been granted.  You may now login to the application.");
-        } catch (MessagingException e) {
-            LOGGER.error("MessagingException occurred sending user activated email.", e);
-        } finally {}
-        javaMailSender.send(mail);
+//        MimeMessage mail = javaMailSender.createMimeMessage();
+//        try {
+//            MimeMessageHelper helper = new MimeMessageHelper(mail, true);
+//            helper.setTo(user.getEmail());
+//            helper.setReplyTo(ADMIN_EMAIL);
+//            helper.setFrom(ADMIN_EMAIL);
+//            helper.setSubject("CCC User Access Granted");
+//            helper.setText("Your access request has been granted.  You may now login to the application.");
+//        } catch (MessagingException e) {
+//            LOGGER.error("MessagingException occurred sending user activated email.", e);
+//        } finally {}
+//        javaMailSender.send(mail);
+
+        Email from = new Email(ADMIN_EMAIL);
+        String subject = "CCC User Access Granted";
+        Email to = new Email(user.getEmail());
+        Content content = new Content("text/plain", "Your access request has been granted.  You may now login to the application.");
+        Mail mail = new Mail(from, subject, to, content);
+
+        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+        Request request = new Request();
+        sendMail(mail, sg, request);
     }
 
     // send a user creation email to admin
     private void sendUserCreationEmail(UserCreateForm user) {
-        MimeMessage mail = javaMailSender.createMimeMessage();
+//        MimeMessage mail = javaMailSender.createMimeMessage();
+//        try {
+//            MimeMessageHelper helper = new MimeMessageHelper(mail, true);
+//            helper.setTo(ADMIN_EMAIL);
+//            helper.setReplyTo(ADMIN_EMAIL);
+//            helper.setFrom(EMAIL_SENDER);
+//            helper.setSubject("CCC User Created");
+//            helper.setText("A new user has registered and requested access to the CCC Database: " + user.getFullName() + ": " + user.getEmail());
+//        } catch (MessagingException e) {
+//            LOGGER.error("MessagingException occurred sending user registration email.", e);
+//        } finally {}
+//        javaMailSender.send(mail);
+
+
+        Email from = new Email(EMAIL_SENDER);
+        String subject = "CCC User Created";
+        Email to = new Email(ADMIN_EMAIL);
+        Content content = new Content("text/plain", "A new user has registered and requested access to the CCC Database: " + user.getFullName() + ": " + user.getEmail());
+        Mail mail = new Mail(from, subject, to, content);
+
+        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+        Request request = new Request();
+        sendMail(mail, sg, request);
+    }
+
+    private void sendMail(Mail mail, SendGrid sg, Request request) {
         try {
-            MimeMessageHelper helper = new MimeMessageHelper(mail, true);
-            helper.setTo(ADMIN_EMAIL);
-            helper.setReplyTo(ADMIN_EMAIL);
-            helper.setFrom(EMAIL_SENDER);
-            helper.setSubject("CCC User Created");
-            helper.setText("A new user has registered and requested access to the CCC Databse: " + user.getFullName() + ": " + user.getEmail());
-        } catch (MessagingException e) {
-            LOGGER.error("MessagingException occurred sending user registration email.", e);
-        } finally {}
-        javaMailSender.send(mail);
+            request.method = Method.POST;
+            request.endpoint = "mail/send";
+            request.body = mail.build();
+            Response response = sg.api(request);
+            LOGGER.debug("Email resp statusCode: " + response.statusCode);
+            LOGGER.debug("Email resp body: " + response.body);
+            LOGGER.debug("Email resp headers: " + response.headers);
+        } catch (IOException ex) {
+            LOGGER.error("MessagingException occurred sending user activated email.", ex);
+        }
     }
 }
